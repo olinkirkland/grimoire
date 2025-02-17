f28d17b98f332b69acf0551f4d467d9439fcf85a
Olin Kirkland <olin.kirkland@beg.de>
2025-02-17 15:50:16 +0100

    Add base url to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [vue()],
     server: { host: false }, // For external IP access
+    base: '/grimoire',
     resolve: {
         alias: {
             '@': new URL('./src', import.meta.url).pathname
