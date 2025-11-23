9d83e38fba605f17ba092e9e6c400ec365b9443a
Olin Kirkland <me@olinkirk.land>
2025-11-23 12:16:39 -0500

    Revert "Prep client code for monorepo"
    This reverts commit a3db19d859040c79098364719879fcc7b51c1b2f.

---
+++ src/router/index.ts
@@ -0,0 +1,81 @@
+import TheAdventurerPage from '@/components/pages/TheAdventurerPage.vue';
+import TheHomePage from '@/components/pages/TheHomePage.vue';
+import TheImportPage from '@/components/pages/TheImportPage.vue';
+import TheLostPage from '@/components/pages/TheLostPage.vue';
+import { startTracking, stopTracking } from '@/tracker';
+import { RouterOptions, createRouter, createWebHistory } from 'vue-router';
+
+export const PageName = {
+    HOME: 'home-page',
+    ADVENTURER: 'adventurer-page',
+    ADVENTURER_STEP: 'adventurer-step-page',
+    LOST: 'lost-page',
+    IMPORT: 'import-page'
+};
+
+const routes = [
+    {
+        path: '/',
+        components: {
+            page: TheHomePage
+        },
+        name: PageName.HOME
+    },
+    {
+        path: '/a/:id',
+        components: {
+            page: TheAdventurerPage
+        },
+        name: PageName.ADVENTURER
+    },
+    {
+        path: '/a/:id/:step',
+        components: {
+            page: TheAdventurerPage
+        },
+        name: PageName.ADVENTURER_STEP
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        components: {
+            page: TheLostPage // 404 page
+        },
+        name: PageName.LOST
+    },
+    {
+        // Import a base64 encoded JSON string of the adventurer data
+        path: '/i/:data',
+        components: {
+            page: TheImportPage
+        },
+        name: PageName.IMPORT
+    }
+];
+
+export const BASE_URL = '/grimoire/';
+const routerOptions = {
+    history: createWebHistory(BASE_URL),
+    routes
+};
+
+export const router = createRouter(routerOptions as RouterOptions);
+
+router.beforeEach(async (to, from, next) => {
+    // Only track if the user has allowed it (localStorage)
+
+    let allowTracking: boolean = false;
+
+    // If there's no tracking key in localStorage, set it to true
+    if (localStorage.getItem('tracking') === null) localStorage.setItem('tracking', 'true');
+
+    // User has allowed tracking
+    if (localStorage.getItem('tracking') === 'true') allowTracking = true;
+
+    // User has denied tracking
+    if (window.location.hostname === 'localhost') allowTracking = false;
+
+    if (allowTracking) startTracking();
+    else stopTracking();
+
+    next();
+});
